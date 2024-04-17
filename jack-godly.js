d48e44b116ec7230bf4bfd7c1acb9d9fb60f4cd9
aconn1234567890 <162541978+aconn1234567890@users.noreply.github.com>
2024-04-17 01:25:20 -0500

    Fixing quote

--- jack-godly.js
+++ jack-godly.js
@@ -1,5 +1,5 @@
 if (touser.toLowerCase() == "jackizgodly") {
   "JackizGodly iz 100% godly jackizCrown"
 } else {
-  `${touser} iz ${Math.ceil(Math.random()*100)}% godly jackizCrown"
+  `${touser} iz ${Math.ceil(Math.random()*100)}% godly jackizCrown`
 }
